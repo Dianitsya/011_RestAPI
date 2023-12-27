d285abf306d1f61695228b5c0f8be89368a22c15
Dianitsya <dianitasyanandamasta@gmail.com>
2023-12-27 23:04:08 +0700

    mengedit home view

--- app/src/main/java/com/example/consumerestapi/ui/home/kontak/screen/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/kontak/screen/viewmodel/HomeViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.consumerestapi.ui.home.viewmodel
+package com.example.consumerestapi.ui.home.kontak.screen.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
