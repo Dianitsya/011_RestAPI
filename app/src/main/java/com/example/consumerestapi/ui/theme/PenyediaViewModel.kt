80794fa6de29e3129c9e9997a6bd0e7a1f7318b5
Dianitsya <dianitasyanandamasta@gmail.com>
2023-12-27 22:34:28 +0700

    inntital commit

---
+++ app/src/main/java/com/example/consumerestapi/ui/theme/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.consumerestapi.ui.theme
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.KontakAplikation
+import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiKontak().container.KontakRepository)
+        }
+    }
+}
+fun CreationExtras.aplikasiKontak(): KontakAplikation =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakAplikation)
