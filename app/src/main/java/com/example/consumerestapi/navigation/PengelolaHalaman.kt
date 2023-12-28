0ccd0c918362269418f922547c5149851f164e86
Dianitsya <dianitasyanandamasta@gmail.com>
2023-12-28 21:53:26 +0700

    memperbaiki home screen

--- app/src/main/java/com/example/consumerestapi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/consumerestapi/navigation/PengelolaHalaman.kt
@@ -2,20 +2,40 @@ package com.example.consumerestapi.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
-import com.example.consumerestapi.ui.home.kontak.screen.HomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.consumerestapi.ui.kontak.screen.DestinasiEntry
+import com.example.consumerestapi.ui.kontak.screen.DestinasiHome
+import com.example.consumerestapi.ui.kontak.screen.EntryKontakScreen
+import com.example.consumerestapi.ui.kontak.screen.HomeScreen
 
 @Composable
-fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController()
+) {
     NavHost(
         navController = navController,
         startDestination = DestinasiHome.route,
         modifier = Modifier,
     ){
         composable(DestinasiHome.route){
-            HomeScreen(naviateToItemEntry = {
-            }
+            HomeScreen(navigateToItemEntry = {
+                navController.navigate(DestinasiEntry.route)
+            },
+                onDetailClick = {})
+        }
+        composable(DestinasiEntry.route){
+            EntryKontakScreen(navigateBack = {
+                navController.navigate(
+                    DestinasiHome.route
+                ){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
         }
     }
-})
+}
